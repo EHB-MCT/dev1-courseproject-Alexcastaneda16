75cc1f8ea3bdf43dfb9437bbae5ba4fec23f0aea
alexcastaneda <alex.castaneda.cushcagua@student.ehb.be>
2024-10-04 14:48:10 +0200

    done

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,26 @@
 "use strict";
+signature()
+function signature(){
+    let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+   context.fillRect(0,0,300,300)
+
+   context.fillStyle = "purple";
+   context.fillRect(25,25,250,50)    
+   context.fillRect(75,75,150,50)
+   context.fillRect(125,125,50,50)
+   context.fillRect(125,125,50,150)
+   context.fillRect(25,125,50,50)
+   context.fillRect(225,125,50,50)
+
+
+
+
+ 
+
+
+
+}
